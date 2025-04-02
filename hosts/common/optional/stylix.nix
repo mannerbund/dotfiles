747e8fafc39452d3cee02cf40b17c3c02fefe1b3
apostolic <sedativechan@gmail.com>
2025-04-02 09:36:52 +0300

    wallpapers managed by swww added

--- hosts/common/optional/stylix.nix
+++ hosts/common/optional/stylix.nix
@@ -4,10 +4,6 @@
   ...
 }: let
   theme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-  wallpaper = pkgs.runCommand "image.png" {} ''
-    COLOR=$(${pkgs.yq}/bin/yq -r .palette.base00 ${theme})
-    ${pkgs.imagemagick}/bin/magick -size 1920x1080 xc:$COLOR $out
-  '';
 in {
   imports = [inputs.stylix.nixosModules.stylix];
 
@@ -23,7 +19,6 @@ in {
     enable = true;
     homeManagerIntegration.followSystem = true;
     base16Scheme = theme;
-    image = wallpaper;
     polarity = "dark";
     fonts.sizes = {
       terminal = 16;
