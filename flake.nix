b72c8594e5e9e11741e50a6c380eeaa738cd428f
mannerbund <apostalimus@gmail.com>
2025-05-19 14:18:49 +0300

    inputs: nix-community cache & bump

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "ICE";
 
+  nixConfig = {
+    extra-substituters = "https://nix-community.cachix.org";
+    extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
