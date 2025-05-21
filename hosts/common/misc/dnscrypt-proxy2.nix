c685cfaf4997f58ea1e5d43d58b78d2a409d8efe
mannerbund <apostalimus@gmail.com>
2025-05-21 17:40:33 +0300

    fix(dnscrypt-proxy2): cache file creates properly

--- hosts/common/misc/dnscrypt-proxy2.nix
+++ hosts/common/misc/dnscrypt-proxy2.nix
@@ -1,14 +1,21 @@
+let
+  StateDirectory = "dnscrypt-proxy";
+in
 {
   services.dnscrypt-proxy2 = {
     enable = true;
     settings = {
+      listen_addresses = [ "127.0.0.1:53" ];
       require_dnssec = true;
+      require_nofilter = true;
+      require_nolog = false;
+      block_ipv6 = true;
       sources.public-resolvers = {
         urls = [
           "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
           "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
         ];
-        cache_file = "public-resolvers.md";
+        cache_file = "/var/lib/${StateDirectory}/public-resolvers.md";
         minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
         refresh_delay = 72;
       };
