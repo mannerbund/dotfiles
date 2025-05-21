7aaaaa47658700bd7af3000db49f3a7c2b1802ee
mannerbund <apostalimus@gmail.com>
2025-05-21 21:13:45 +0300

    chore

--- hosts/common/core/openssh.nix
+++ hosts/common/core/openssh.nix
@@ -1,20 +1,4 @@
 {
-  #inputs,
-  #lib,
-  #config,
-  ...
-}: {
-  #programs.ssh.startAgent = true;
-  #services.openssh = {
-  #  enable = true;
-  #  settings = {
-  #    PasswordAuthentication = true;
-  #    PermitRootLogin = "no";
-  #    AcceptEnv = "WAYLAND_DISPLAY";
-  #    X11Forwarding = true;
-  #  };
-  #};
-
   programs.ssh = {
     startAgent = true;
   };
