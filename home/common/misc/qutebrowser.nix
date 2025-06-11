e047d283efa5bac037fb4337699fb43e531f391f
mannerbund <apostalimus@gmail.com>
2025-06-11 20:12:10 +0300

    pkgs: qutebrowser added

---
+++ home/common/misc/qutebrowser.nix
@@ -0,0 +1,24 @@
+{
+  stylix.targets.qutebrowser.enable = true;
+
+  home.sessionVariables = {
+    BROWSER = "qutebrowser";
+  };
+
+  programs.qutebrowser = {
+    enable = true;
+    quickmarks = {
+      github = "https://github.com";
+      chatgpt = "https://chatgpt.com";
+      nixos-search = "https://search.nixos.org";
+      home-manager = "https://home-manager-options.extranix.com/?query=&release=master";
+      libgen = "https://libgen.rs";
+      annas-archive = "https://annas-archive.org";
+      audiobookbay = "https://audiobookbay.lu";
+      z-lib = "https://1lib.sk";
+      flibusta = "https://flibusta.is";
+      rutracker = "https://rutracker.org";
+      thepiratebay = "https://thepiratebay.org";
+    };
+  };
+}
