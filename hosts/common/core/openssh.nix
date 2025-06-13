a17bf5b25acc23830a2388faffc22422c746c969
mannerbund <apostalimus@gmail.com>
2025-06-13 19:40:24 +0300

    format

--- hosts/common/core/openssh.nix
+++ hosts/common/core/openssh.nix
@@ -1,12 +1,12 @@
 {
   services.openssh = {
     enable = true;
-    ports = [ 58530 ];
+    ports = [58530];
     settings = {
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
       PermitRootLogin = "no";
-      AllowUsers = [ "apostolic" ];
+      AllowUsers = ["apostolic"];
     };
   };
 
