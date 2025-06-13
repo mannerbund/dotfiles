e0922997f788a11f683a8137ac8392e7e9298684
mannerbund <apostalimus@gmail.com>
2025-06-13 22:53:20 +0300

    irssi: theme added

--- home/common/misc/irssi.nix
+++ home/common/misc/irssi.nix
@@ -1,17 +1,36 @@
-{
+{config, ...}: let
+  gothicTheme = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/phracker/irssi-themes/refs/heads/master/gothic.theme";
+    sha256 = "10whibwj077f6yx89x7mslx1dkrk6fq24m0mwrq5i954acwxg7bw";
+  };
+  nick = "${config.home.username}";
+in {
+  home.file.".irssi/gothic.theme".source = gothicTheme;
+
   programs.irssi = {
     enable = true;
     networks = {
       liberachat = {
-        nick = "apostolic";
+        nick = "${nick}";
         server = {
           address = "irc.libera.chat";
           port = 6697;
           ssl.enable = true;
           ssl.verify = true;
-          autoConnect = true;
+        };
+      };
+      lainchan = {
+        nick = "${nick}";
+        server = {
+          address = "irc.lainchan.org";
+          port = 6697;
+          ssl.enable = true;
+          ssl.verify = true;
         };
       };
     };
+    extraConfig = ''
+      settings = { "fe-common/core" = { theme = "gothic"; }; };
+    '';
   };
 }
