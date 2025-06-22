fd42aab7fddd72cf4bff5cb0eed364ea10e569cc
mannerbund <apostalimus@gmail.com>
2025-06-22 21:06:14 +0300

    lf: image previews disabled

--- home/common/core/lf.nix
+++ home/common/core/lf.nix
@@ -3,25 +3,32 @@
   lib,
   config,
   ...
-}: let
-  eza = lib.getExe pkgs.eza;
-  chafa = lib.getExe pkgs.chafa;
-  mediainfo = lib.getExe pkgs.mediainfo;
-  ffmpegthumb = lib.getExe pkgs.ffmpegthumbnailer;
-  file = lib.getExe pkgs.file;
-  unzip = lib.getExe pkgs.unzip;
-  gzip = lib.getExe pkgs.gzip;
-  unrar = lib.getExe pkgs.unrar;
-  bat = lib.getExe pkgs.bat;
-  epub-thumbnailer = lib.getExe pkgs.epub-thumbnailer;
-  pdftoppm = "${pkgs.poppler_utils}/bin/pdftoppm";
-  z7 = "${pkgs.p7zip}/bin/7z";
-in {
+}: 
+{
   home.persistence."/persist/${config.home.homeDirectory}" = {
     directories = [
-      ".local/share/lf"
+     ".local/share/lf"
     ];
   };
+
+  home.packages = with pkgs; [
+    mediainfo
+    file
+    unzip
+    gzip
+    unrar
+    bat
+    epub-thumbnailer
+    p7zip
+    xz
+    libcdio
+    odt2txt
+    catdoc
+    gnumeric
+    exiftool
+    man-db
+    groff
+  ];
 
   programs.lf = {
     enable = true;
@@ -58,63 +65,49 @@ in {
 
       # Eza
       cmd on-select &{{
-          lf -remote "send $id set statfmt \"$(${eza} -ld --color=always "$f" | sed 's/\\/\\\\/g;s/"/\\"/g')\""
+          lf -remote "send $id set statfmt \"$(${lib.getExe pkgs.eza} -ld --color=always "$f" | sed 's/\\/\\\\/g;s/"/\\"/g')\""
       }}
     '';
     previewer = {
       keybinding = "i";
       source = pkgs.writeShellScript "pv.sh" ''
         #!/bin/sh
-        CACHE="${config.home.homeDirectory}/.cache/lf/thumbnail.$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' \
-        	-- "$(readlink -f "$1")" | sha256sum | awk '{print $1}'))"
-
-        image() {
-          ${chafa} -f sixel -s "$2x$3" --animate off --polite on "$1"
+        
+        batorcat() {
+            file="$1"
+            shift
+            if command -v bat >/dev/null 2>&1; then
+                bat --color=always --style=plain --pager=never "$file" "$@"
+            else
+                cat "$file"
+            fi
         }
-
-        case "$(readlink -f "$1" | awk '{print tolower($0)}')" in
-          *.tgz|*.tar.gz) tar tzf "$1" ;;
-          *.tar.bz2|*.tbz2) tar tjf "$1" ;;
-          *.tar.xz|*.txz) tar tf "$1" ;;
-          *.tar) tar tf "$1" ;;
-          *.zip|*.jar|*.war|*.ear|*.oxt) ${unzip} -l "$1" ;;
-          *.gz) ${gzip} -l "$1" ;;
-          *.rar) ${unrar} l "$1" ;;
-          *.7z) ${z7} l "$1" ;;
-         *.wav|*.mp3|*.flac|*.m4a|*.wma|*.ape|*.ac3|*.og[agx]|*.spx|*.opus|*.as[fx]|*.mka)
-         	${mediainfo} "$1"
-         	;;
-         *.pdf)
-         	[ ! -f "''${CACHE}.jpg" ] && ${pdftoppm} -jpeg -f 1 -singlefile "$1" "$CACHE"
-            image "''${CACHE}.jpg" "$2" "$3"
-         	;;
-         *.epub)
-         	[ ! -f "$CACHE" ] && ${epub-thumbnailer} "$1" "$CACHE" 1024
-            image "$CACHE" "$2" "$3"
-         	;;
-         *.cbz|*.cbr|*.cbt)
-         	[ ! -f "$CACHE" ] && comicthumb "$1" "$CACHE" 1024
-            image "$CACHE" "$2" "$3"
-         	;;
-         *.avi|*.mp4|*.wmv|*.dat|*.3gp|*.ogv|*.mkv|*.mpg|*.mpeg|*.vob|*.fl[icv]|*.m2v|*.mov|*.webm|*.ts|*.mts|*.m4v|*.r[am]|*.qt|*.divx)
-         	[ ! -f "''${CACHE}.jpg" ] && \
-         		${ffmpegthumb} -i "$1" -o "''${CACHE}.jpg" -s 0 -q 5
-              image "''${CACHE}.jpg" "$2" "$3"
-         	;;
-
-          *)
-            case "$(${file} -Lb --mime-type -- "$1")" in
-              image/*)
-                image "$1" "$2" "$3"
-                exit 0
+        
+        case "$(printf "%s\n" "$(readlink -f "$1")" | tr '[:upper:]' '[:lower:]')" in
+            *.tgz | *.tar.gz) tar tzf "$1" ;;
+            *.tar.bz2 | *.tbz2) tar tjf "$1" ;;
+            *.tar.txz | *.txz) xz --list "$1" ;;
+            *.tar) tar tf "$1" ;;
+            *.zip | *.jar | *.war | *.ear | *.oxt) unzip -l "$1" ;;
+            *.rar) unrar l "$1" ;;
+            *.7z) 7z l "$1" ;;
+            *.[1-8]) man "$1" | col -b ;;
+            *.o) nm "$1" ;;
+            *.torrent) transmission-show "$1" ;;
+            *.iso) iso-info --no-header -l "$1" ;;
+            *.odt | *.ods | *.odp | *.sxw) odt2txt "$1" ;;
+            *.doc) catdoc "$1" ;;
+            *.xls | *.xlsx)
+                ssconvert --export-type=Gnumeric_stf:stf_csv "$1" "fd://1" | batorcat --language=csv
                 ;;
-              text/*)
-                ${bat} --color=always --style=plain --pager=never "$1"
-                exit 0
+            *.wav | *.mp3 | *.flac | *.m4a | *.wma | *.ape | *.ac3 | *.og[agx] | *.spx | *.opus | *.as[fx] | *.mka)
+                exiftool "$1"
                 ;;
-            esac
-            ;;
+            *)
+                batorcat "$1"
+                ;;
         esac
+        exit 0
       '';
     };
   };
