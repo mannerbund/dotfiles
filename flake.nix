686aad8657c73dd77b4fc71f473f32bdb9069b76
mannerbund <apostalimus@gmail.com>
2025-06-22 13:44:44 +0300

    chore

--- flake.nix
+++ flake.nix
@@ -32,10 +32,6 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    #zen-browser = {
-    #  url = "github:0xc000022070/zen-browser-flake";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
   };
 
   outputs = inputs @ {
