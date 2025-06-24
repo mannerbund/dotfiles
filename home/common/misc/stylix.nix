f129bfb9b97103c1561baa00d808dc754757ce62
mannerbund <apostalimus@gmail.com>
2025-06-24 17:37:53 +0300

    stylix: some options moved to home module
    wallpaper moved to home/common directory

--- home/common/misc/stylix.nix
+++ home/common/misc/stylix.nix
@@ -4,6 +4,31 @@
     enable = true;
     autoEnable = false;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark.yaml";
+    cursor = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+      size = 16;
+    };
+    fonts = {
+      sizes = {
+        applications = 14;
+        desktop = 14;
+        popups = 14;
+        terminal = 16;
+      };
+      serif = {
+        name = "Dejavu Serif";
+      };
+      sansSerif = {
+        name = "Dejavu Sans";
+      };
+      monospace = {
+        name = "Aporetic Sans Mono";
+      };
+      emoji = {
+        name = "Noto Color Emoji";
+      };
+    };
     targets = {
       firefox.enable = true;
       vim.enable = true;
