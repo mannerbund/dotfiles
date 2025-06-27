6507eaa51f908a68f83bb70a78dc8a45b0fdd568
mannerbund <apostalimus@gmail.com>
2025-06-27 19:16:23 +0300

    irssi: tmux-nicklist script removed

--- home/common/core/irssi.nix
+++ home/common/core/irssi.nix
@@ -4,10 +4,6 @@
     url = "https://raw.githubusercontent.com/phracker/irssi-themes/refs/heads/master/gothic.theme";
     sha256 = "10whibwj077f6yx89x7mslx1dkrk6fq24m0mwrq5i954acwxg7bw";
   };
-  tmux-nicklist = builtins.fetchurl {
-    url = "https://raw.githubusercontent.com/irssi/scripts.irssi.org/refs/heads/master/scripts/tmux-nicklist-portable.pl";
-    sha256 = "0rpjd1c34xw2psrpq5halq4r1ydg66hq3nk00d9n4kn4jk1mlwbn";
-  };
   trackbar = builtins.fetchurl {
     url = "https://raw.githubusercontent.com/irssi/scripts.irssi.org/refs/heads/master/scripts/trackbar.pl";
     sha256 = "1hhhs2vxmsml4dfng2iq3723fz86b0l6s91l78zg8chmay89ca4l";
@@ -18,7 +14,6 @@
   };
 in {
   home.file.".irssi/gothic.theme".source = gothicTheme;
-  home.file.".irssi/scripts/autorun/tmux-nicklist-portable.pl".source = tmux-nicklist;
   home.file.".irssi/scripts/autorun/trackbar.pl".source = trackbar;
   home.file.".irssi/scripts/autorun/nickcolor.pl".source = nickcolor;
 
