0a9854f7c6f590d821a81b726239c98054660b4d
mannerbund <apostalimus@gmail.com>
2025-07-07 00:26:44 +0300

    lf(feat): previews with ueberzugpp

--- home/common/core/lf.nix
+++ home/common/core/lf.nix
@@ -3,7 +3,8 @@
   lib,
   config,
   ...
-}: {
+}:
+{
   home.persistence."/persist/${config.home.homeDirectory}" = {
     directories = [
       ".local/share/lf"
@@ -14,10 +15,9 @@
     mediainfo
     file
     unzip
-    gzip
     unrar
     bat
-    epub-thumbnailer
+    gnome-epub-thumbnailer
     p7zip
     xz
     libcdio
@@ -27,7 +27,62 @@
     exiftool
     man-db
     groff
+    ueberzugpp
+    ffmpegthumbnailer
+    poppler-utils
+    imagemagick
   ];
+
+  home.file = {
+    ".config/lf/cleaner" = {
+      executable = true;
+      text = ''
+        #!/bin/sh
+
+        ueberzugpp cmd -s $UB_SOCKET -a remove -i PREVIEW
+      '';
+    };
+    ".local/bin/lfub" = {
+      executable = true;
+      text = ''
+        #!/bin/sh
+        
+        # This is a wrapper script for lf that allows it to create image previews with
+        # ueberzug. This works in concert with the lf configuration file and the
+        # lf-cleaner script.
+        
+        set -e
+        
+        UB_PID=0
+        UB_SOCKET=""
+        
+        case "$(uname -a)" in
+            *Darwin*) UEBERZUG_TMP_DIR="$TMPDIR" ;;
+            *) UEBERZUG_TMP_DIR="/tmp" ;;
+        esac
+        
+        cleanup() {
+            exec 3>&-
+            ueberzugpp cmd -s "$UB_SOCKET" -a exit
+        }
+        
+        if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+            lf "$@"
+        else
+            [ ! -d "$HOME/.cache/lf" ] && mkdir -p "$HOME/.cache/lf"
+            UB_PID_FILE="$UEBERZUG_TMP_DIR/.$(uuidgen)"
+            ueberzugpp layer --silent --no-stdin --use-escape-codes --pid-file "$UB_PID_FILE"
+            UB_PID=$(cat "$UB_PID_FILE")
+            rm "$UB_PID_FILE"
+            UB_SOCKET="$UEBERZUG_TMP_DIR/ueberzugpp-''${UB_PID}.socket"
+            export UB_PID UB_SOCKET
+            trap cleanup HUP INT QUIT TERM EXIT
+            lf "$@" 3>&-
+        fi
+
+      '';
+    };
+  };
 
   programs.lf = {
     enable = true;
@@ -36,6 +91,7 @@
       ignorecase = true;
       sixel = true;
       tabstop = 4;
+      cleaner = "~/.config/lf/cleaner";
     };
     keybindings = {
       "." = "set hidden!";
@@ -71,7 +127,17 @@
       keybinding = "i";
       source = pkgs.writeShellScript "pv.sh" ''
         #!/bin/sh
-
+        
+        image() {
+            FILE_PATH="$1"
+            X=$4
+            Y=$5
+            MW=$(($2 - 1))
+            MH=$3
+            ueberzugpp cmd -s "$UB_SOCKET" -a add -i PREVIEW -x "$X" -y "$Y" --max-width "$MW" --max-height "$MH" -f "$FILE_PATH"
+            exit 1
+        }
+        
         batorcat() {
             file="$1"
             shift
@@ -81,7 +147,9 @@
                 cat "$file"
             fi
         }
-
+        
+        CACHE="$HOME/.cache/lf/thumbnail.$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | awk '{print $1}'))"
+        
         case "$(printf "%s\n" "$(readlink -f "$1")" | tr '[:upper:]' '[:lower:]')" in
             *.tgz | *.tar.gz) tar tzf "$1" ;;
             *.tar.bz2 | *.tbz2) tar tjf "$1" ;;
@@ -96,12 +164,33 @@
             *.iso) iso-info --no-header -l "$1" ;;
             *.odt | *.ods | *.odp | *.sxw) odt2txt "$1" ;;
             *.doc) catdoc "$1" ;;
+            *.docx) docx2txt "$1" - ;;
             *.xls | *.xlsx)
                 ssconvert --export-type=Gnumeric_stf:stf_csv "$1" "fd://1" | batorcat --language=csv
                 ;;
             *.wav | *.mp3 | *.flac | *.m4a | *.wma | *.ape | *.ac3 | *.og[agx] | *.spx | *.opus | *.as[fx] | *.mka)
                 exiftool "$1"
                 ;;
+            *.pdf)
+                [ ! -f "''${CACHE}.jpg" ] && pdftoppm -jpeg -f 1 -singlefile "$1" "$CACHE"
+                image "''${CACHE}.jpg" "$2" "$3" "$4" "$5"
+                ;;
+	        */epub+zip|*/mobi*)
+                [ ! -f "''${CACHE}.jpg" ] && gnome-epub-thumbnailer "$1" "$CACHE"
+                image "''${CACHE}.jpg" "$2" "$3" "$4" "$5"
+                ;;
+            *.avi | *.mp4 | *.wmv | *.dat | *.3gp | *.ogv | *.mkv | *.mpg | *.mpeg | *.vob | *.fl[icv] | *.m2v | *.mov | *.webm | *.ts | *.mts | *.m4v | *.r[am] | *.qt | *.divx)
+                [ ! -f "''${CACHE}.jpg" ] && ffmpegthumbnailer -i "$1" -o "''${CACHE}.jpg" -s 0 -q 5
+                image "''${CACHE}.jpg" "$2" "$3" "$4" "$5"
+                ;;
+            *.bmp | *.jpg | *.jpeg | *.png | *.xpm | *.webp | *.gif | *.jfif)
+                image "$1" "$2" "$3" "$4" "$5"
+                ;;
+            *.svg)
+                [ ! -f "''${CACHE}.jpg" ] && convert "$1" "''${CACHE}.jpg"
+                image "''${CACHE}.jpg" "$2" "$3" "$4" "$5"
+                ;;
+
             *)
                 batorcat "$1"
                 ;;
