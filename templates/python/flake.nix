de9164e723415734006734f0f49d0adb17ff5e11
mannerbund <apostalimus@gmail.com>
2025-07-11 11:05:20 +0300

    inputs: bump

--- templates/python/flake.nix
+++ templates/python/flake.nix
@@ -10,8 +10,7 @@
   }: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
   in {
-    devShells.x86_64-linux.default = with pkgs;
-      mkShell {
+    devShells.x86_64-linux.default = pkgs.mkShell {
         packages = [
           (pkgs.python313.withPackages (python-pkgs:
             with python-pkgs; [
