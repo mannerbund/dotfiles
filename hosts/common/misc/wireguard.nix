e8c4784ea0f2797a14ae4fe41fd2973b1e1788b6
mannerbund <apostalimus@gmail.com>
2025-07-16 06:17:12 +0300

    wg: bump

--- hosts/common/misc/wireguard.nix
+++ hosts/common/misc/wireguard.nix
@@ -10,15 +10,15 @@
 
   networking.wg-quick.interfaces = {
     wg0 = {
-      address = ["192.168.6.68/32"];
+      address = ["192.168.6.113/32"];
       dns = ["127.0.0.1"];
       privateKeyFile = "/var/lib/wireguard/wireguard-privkey";
 
       peers = [
         {
-          publicKey = "OxuczEDPtabvj+hACzXuUpvRIs0IVWyrHzT4j0qcHj8=";
+          publicKey = "FeveuhtZzyHHrza13sIMnsywtRYXZpMDTCnQ1NXbsA8=";
           allowedIPs = ["0.0.0.0/0"];
-          endpoint = "92.118.170.240:1024";
+          endpoint = "109.206.241.94:1024";
           persistentKeepalive = 25;
         }
       ];
