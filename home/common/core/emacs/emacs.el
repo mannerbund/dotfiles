acf2b0fe8a40bf9364203641ee64fadfac7a3e3d
mannerbund <apostalimus@gmail.com>
2025-07-20 23:19:53 +0300

    emacs(pkgs): indent-bars removed

--- home/common/core/emacs/emacs.el
+++ home/common/core/emacs/emacs.el
@@ -537,22 +537,6 @@
   (text-mode . hl-line-mode)
   (org-mode . (lambda () (hl-line-mode -1))))
 
-(use-package indent-bars
-  :ensure t
-  :custom
-  (indent-bars-no-descend-lists t) ; no extra bars in continued func arg lists
-  (indent-bars-treesit-support t)
-  (indent-bars-treesit-ignore-blank-lines-types '("module"))
-  ;; Add other languages as needed
-  (indent-bars-treesit-scope '((python function_definition class_definition for_statement
-	                                   if_statement with_statement while_statement)))
-  ;; Note: wrap may not be needed if no-descend-list is enough
-  ;;(indent-bars-treesit-wrap '((python argument_list parameters ; for python, as an example
-  ;;				      list list_comprehension
-  ;;				      dictionary dictionary_comprehension
-  ;;				      parenthesized_expression subscript)))
-  :hook ((python-base-mode yaml-mode) . indent-bars-mode))
-
 (use-package eldoc
   :config
   (setopt eldoc-idle-delay 0.5)
