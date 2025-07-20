25b8e51ad6309b42b13386fa62f056caabe6bdc1
mannerbund <apostalimus@gmail.com>
2025-07-20 23:17:40 +0300

    emacs(pkgs): eglot-booster package bump

--- home/common/core/emacs/eglot-booster.nix
+++ home/common/core/emacs/eglot-booster.nix
@@ -4,11 +4,11 @@
 }:
 trivialBuild {
   pname = "eglot-booster";
-  version = "042825";
+  version = "071625";
   src = fetchFromGitHub {
     owner = "jdtsmith";
     repo = "eglot-booster";
-    rev = "1260d2f7dd18619b42359aa3e1ba6871aa52fd26";
-    sha256 = "0b8pknnkyzqmi7b8ms27dzcbcx87cn2m8m160v18mv6b61c0mq5m";
+    rev = "cab7803c4f0adc7fff9da6680f90110674bb7a22";
+    sha256 = "xUBQrQpw+JZxcqT1fy/8C2tjKwa7sLFHXamBm45Fa4Y=";
   };
 }
