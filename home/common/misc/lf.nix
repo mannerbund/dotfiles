5b32f5680c81515bc411fb69a481370a7509701d
mannerbund <apostalimus@gmail.com>
2025-08-14 01:13:54 +0300

    lf(fix): gnome-epub-thumbnailer proper image format

--- home/common/misc/lf.nix
+++ home/common/misc/lf.nix
@@ -144,7 +144,7 @@
             fi
         }
 
-        CACHE="$HOME/.cache/lf/thumbnail.$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | awk '{print $1}'))"
+        CACHE="$HOME/.cache/lf/thumbnail.$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | awk '{print $1}')"
 
         case "$(printf "%s\n" "$(readlink -f "$1")" | tr '[:upper:]' '[:lower:]')" in
             *.tgz | *.tar.gz) tar tzf "$1" ;;
@@ -171,10 +171,11 @@
                 [ ! -f "''${CACHE}.jpg" ] && pdftoppm -jpeg -f 1 -singlefile "$1" "$CACHE"
                 image "''${CACHE}.jpg" "$2" "$3" "$4" "$5"
                 ;;
-            *.epub | *.mobi)
-                [ ! -f "''${CACHE}.jpg" ] && gnome-epub-thumbnailer "$1" "$CACHE"
-                image "''${CACHE}.jpg" "$2" "$3" "$4" "$5"
+            *.epub)
+                [ ! -f "''${CACHE}.png" ] && gnome-epub-thumbnailer "$1" "''${CACHE}.png"
+                image "''${CACHE}.png" "$2" "$3" "$4" "$5"
                 ;;
+
             *.avi | *.mp4 | *.wmv | *.dat | *.3gp | *.ogv | *.mkv | *.mpg | *.mpeg | *.vob | *.fl[icv] | *.m2v | *.mov | *.webm | *.ts | *.mts | *.m4v | *.r[am] | *.qt | *.divx)
                 [ ! -f "''${CACHE}.jpg" ] && ffmpegthumbnailer -i "$1" -o "''${CACHE}.jpg" -s 0 -q 5
                 image "''${CACHE}.jpg" "$2" "$3" "$4" "$5"
