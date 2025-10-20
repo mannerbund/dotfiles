4a4b454fb70f14790f81e971cfceb9c26d54f311
mannerbund <apostalimus@gmail.com>
2025-10-20 19:37:38 +0300

    nix: binary cache in proper place now, no need to add trusted-users
    No need to add `trusted-users`, because `wheel` group is already trusted

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -22,15 +22,6 @@
     mutableUsers = false;
   };
 
-  nix.settings = {
-    substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   nixpkgs = {
     overlays = [
       inputs.self.overlays.default
--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -10,6 +10,12 @@
         "root"
         "@wheel"
       ];
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     optimise.automatic = true;
     gc = {
