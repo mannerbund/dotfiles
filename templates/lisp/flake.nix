6d80320a76bdfa5532de8b51c3f21d97239c1690
mannerbund <apostalimus@gmail.com>
2025-11-14 19:06:43 +0300

    templates: lisp template added

---
+++ templates/lisp/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Lisp";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            sbcl
+          ];
+        };
+      };
+    };
+}
