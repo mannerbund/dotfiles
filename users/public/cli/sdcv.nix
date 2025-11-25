8eab305f3d00b5a99ae3f1caa81565399e1fc0c9
mannerbund <apostalimus@gmail.com>
2025-11-25 20:28:09 +0300

    dotfiles: sdcv proper configuration

--- users/public/cli/sdcv.nix
+++ users/public/cli/sdcv.nix
@@ -1,45 +1,54 @@
 {
-  config,
-  pkgs,
-  lib,
+  username,
   ...
 }:
 {
-  home.persistence."/persist/${config.home.homeDirectory}" = {
-    directories = [
-      ".local/share/stardict"
-    ];
-  };
+  home-manager.users.${username} =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    {
 
-  home.sessionVariables = {
-    STARDICT_DATA_DIR = "${config.home.homeDirectory}/.local/share/stardict";
-    SDCV_HIST = "${config.home.homeDirectory}/.local/share/stardict/sdcv_hist";
-  };
+      home.persistence."/persist/${config.home.homeDirectory}" = {
+        directories = [
+          ".local/share/stardict"
+        ];
+      };
 
-  home.packages = [
-    pkgs.sdcv
-    pkgs.html2text
-  ];
+      home = {
+        packages = [
+          pkgs.sdcv
+          pkgs.html2text
+        ];
+        sessionVariables = {
+          STARDICT_DATA_DIR = "${config.home.homeDirectory}/.local/share/stardict";
+          SDCV_HIST = "${config.home.homeDirectory}/.local/share/stardict/sdcv_hist";
+        };
+      };
 
-  programs.zsh = {
-    shellAliases = {
-      sd = "sdcv -c";
+      programs.zsh = {
+        shellAliases = {
+          sd = "sdcv -c";
+        };
+        initContent = lib.mkOrder 1100 ''
+          oed() {
+            sdcv -0 -1 -n -c -u oed2 "$@" 2>&1 \
+            | html2text -utf8 -width 50
+          }
+
+          soed() {
+            sdcv -0 -1 -n -c -u soed6 "$@" 2>&1 \
+            | html2text -utf8 -width 50
+          }
+
+          sdr() {
+            sdcv -0 -1 -n -c -u ru-en "$@" 2>&1 \
+            | html2text -utf8 -width 50
+          }
+        '';
+      };
     };
-    initContent = lib.mkOrder 1100 ''
-      oed() {
-        sdcv -0 -1 -n -c -u oed2 "$@" 2>&1 \
-        | html2text -utf8 -width 50
-      }
-
-      soed() {
-        sdcv -0 -1 -n -c -u soed6 "$@" 2>&1 \
-        | html2text -utf8 -width 50
-      }
-
-      sdr() {
-        sdcv -0 -1 -n -c -u ru-en "$@" 2>&1 \
-        | html2text -utf8 -width 50
-      }
-    '';
-  };
 }
