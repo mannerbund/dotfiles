3cc7c66536f5e861086bcf994f95585e1dc4c3b3
mannerbund <apostalimus@gmail.com>
2025-12-01 14:30:06 +0300

    formatted

--- users/public/net/syncthing.nix
+++ users/public/net/syncthing.nix
@@ -1,43 +1,45 @@
 { username, ... }:
 {
-  home-manager.users.${username} = {config,...}:{
-    home.persistence."/persist/${config.home.homeDirectory}" = {
-      directories = [
-        ".local/state/syncthing"
-      ];
-    };
+  home-manager.users.${username} =
+    { config, ... }:
+    {
+      home.persistence."/persist/${config.home.homeDirectory}" = {
+        directories = [
+          ".local/state/syncthing"
+        ];
+      };
 
-    services.syncthing = {
-      enable = true;
-      settings = {
-        devices = {
-          phone = {
-            name = "Pixel";
-            id = "SB5OW4W-6LJXBFW-6CYZ7WG-RO643HZ-44UAZ67-DB4AELP-TAGLOUU-OS3G7QM";
+      services.syncthing = {
+        enable = true;
+        settings = {
+          devices = {
+            phone = {
+              name = "Pixel";
+              id = "SB5OW4W-6LJXBFW-6CYZ7WG-RO643HZ-44UAZ67-DB4AELP-TAGLOUU-OS3G7QM";
+            };
           };
-        };
-        folders = {
-          documents = {
-            path = "~/Documents";
-            devices = [ "phone" ];          
+          folders = {
+            documents = {
+              path = "~/Documents";
+              devices = [ "phone" ];
+            };
+            library = {
+              path = "~/Library";
+              devices = [ "phone" ];
+            };
+            music = {
+              path = "~/Music";
+              devices = [ "phone" ];
+            };
           };
-          library = {
-            path = "~/Library";
-            devices = [ "phone" ];
+          options = {
+            localAnnounceEnabled = true;
+            progressUpdateIntervalS = false;
+            globalAnnounceEnabled = false;
+            relaysEnabled = false;
+            natEnabled = false;
           };
-          music = {
-            path = "~/Music";
-            devices = [ "phone" ];
-          };
-        };
-        options = {
-          localAnnounceEnabled = true;
-          progressUpdateIntervalS = false;
-          globalAnnounceEnabled = false;
-          relaysEnabled = false;
-          natEnabled = false;
         };
       };
     };
-  };
 }
