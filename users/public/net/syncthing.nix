1d76a06bc6516fff44b0e5e4b0b4b626c37f3dcb
mannerbund <apostalimus@gmail.com>
2025-12-01 13:49:58 +0300

    users: syncthing configured

---
+++ users/public/net/syncthing.nix
@@ -0,0 +1,43 @@
+{ username, ... }:
+{
+  home-manager.users.${username} = {config,...}:{
+    home.persistence."/persist/${config.home.homeDirectory}" = {
+      directories = [
+        ".local/state/syncthing"
+      ];
+    };
+
+    services.syncthing = {
+      enable = true;
+      settings = {
+        devices = {
+          phone = {
+            name = "Pixel";
+            id = "SB5OW4W-6LJXBFW-6CYZ7WG-RO643HZ-44UAZ67-DB4AELP-TAGLOUU-OS3G7QM";
+          };
+        };
+        folders = {
+          documents = {
+            path = "~/Documents";
+            devices = [ "phone" ];          
+          };
+          library = {
+            path = "~/Library";
+            devices = [ "phone" ];
+          };
+          music = {
+            path = "~/Music";
+            devices = [ "phone" ];
+          };
+        };
+        options = {
+          localAnnounceEnabled = true;
+          progressUpdateIntervalS = false;
+          globalAnnounceEnabled = false;
+          relaysEnabled = false;
+          natEnabled = false;
+        };
+      };
+    };
+  };
+}
