f967892fcaeee62923637886e46c62bd9875b19d
mannerbund <apostalimus@gmail.com>
2025-12-31 17:07:13 +0300

    templates: java template added

---
+++ templates/java/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Java";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+
+  outputs =
+    inputs@{
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              jdk
+              jdt-language-server
+            ];
+          };
+        };
+    };
+}
