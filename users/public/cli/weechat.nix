7ea2c9649d5778202cbe03f5c244e7b9b505054b
mannerbund <apostalimus@gmail.com>
2026-02-04 15:33:30 +0300

    weechat: scripts for weechat

--- users/public/cli/weechat.nix
+++ users/public/cli/weechat.nix
@@ -1,14 +1,31 @@
 {
-  pkgs,
+  username,
   ...
 }:
 {
-  home.persistence."/persist" = {
-    directories = [
-      ".config/weechat"
-      ".local/share/weechat"
-    ];
-  };
 
-  home.packages = [ pkgs.weechat ];
+  home-manager.users.${username} =
+    { pkgs, ... }:
+    {
+
+      home.persistence."/persist" = {
+        directories = [
+          ".config/weechat"
+          ".local/share/weechat"
+        ];
+      };
+
+      home.packages = with pkgs; [
+        (weechat.override {
+          configure =
+            { ... }:
+            {
+              scripts = with pkgs.weechatScripts; [
+                autosort
+                colorize_nicks
+              ];
+            };
+        })
+      ];
+    };
 }
