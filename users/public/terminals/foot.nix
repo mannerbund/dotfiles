91675aa1451ea581dfb7c2cda4df87f0b7beb3b3
mannerbund <apostalimus@gmail.com>
2026-05-10 22:56:43 +0300

    foot: binds change, server enabled

--- users/public/terminals/foot.nix
+++ users/public/terminals/foot.nix
@@ -11,17 +11,18 @@
   home-manager.users.${username} = {
     programs.foot = {
       enable = true;
+      server.enable = true;
       settings = {
         main = {
           term = "xterm-256color";
           font = "Aporetic Sans Mono:size=14";
         };
         key-bindings = {
-          clipboard-copy = "Control+w XF86Copy";
-          clipboard-paste = "Control+y XF86Paste";
-          scrollback-up-half-page = "Control+k";
-          scrollback-down-half-page = "Control+j";
-          show-urls-copy = "Control+Shift+y";
+          clipboard-copy = "Alt+c XF86Copy";
+          clipboard-paste = "Alt+v XF86Paste";
+          scrollback-up-half-page = "Alt+k";
+          scrollback-down-half-page = "Alt+j";
+          show-urls-copy = "Alt+y";
         };
         mouse = {
           hide-when-typing = "yes";
