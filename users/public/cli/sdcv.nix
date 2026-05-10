1cc3fd22bd44601989cb695afef5be180a7e2855
mannerbund <apostalimus@gmail.com>
2026-05-10 22:56:01 +0300

    sdcv: dal's dictionary added, width increased

--- users/public/cli/sdcv.nix
+++ users/public/cli/sdcv.nix
@@ -36,22 +36,25 @@
         initContent = lib.mkOrder 1100 ''
           oed() {
             sdcv -0 -1 -n -c -u oed2 "$@" 2>&1 \
-            | html2text -utf8 -width 50
+            | html2text -utf8 -width 75
           }
 
           soed() {
             sdcv -0 -1 -n -c -u soed6 "$@" 2>&1 \
-            | html2text -utf8 -width 50
-          }
-
-          sdr() {
-            sdcv -0 -1 -n -c -u ru-en "$@" 2>&1 \
-            | html2text -utf8 -width 50
+            | html2text -utf8 -width 75
           }
 
           sdeu() {
             sdcv -0 -1 -n -c -u duden "$@" 2>&1 \
-            | html2text -utf8 -width 50
+            | html2text -utf8 -width 75
+          }
+
+          sdal() {
+            sdcv -0 -1 -n -c -u dal "$@" 2>&1
+          }
+
+          sdalabbrev() {
+            sdcv -0 -1 -n -c -u dalabbrev "$@" 2>&1
           }
         '';
       };
