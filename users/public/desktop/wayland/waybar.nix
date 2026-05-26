24ecacff51e9ea8e66ba750544d54db9d5b36781
mannerbund <apostalimus@gmail.com>
2026-05-26 21:40:58 +0300

    waybar: primitive config for mangowm statusbar

--- users/public/desktop/wayland/waybar.nix
+++ users/public/desktop/wayland/waybar.nix
@@ -1,153 +1,177 @@
 {
-  pkgs,
-  lib,
-  config,
+  username,
   ...
 }:
-let
-  mkSpan = abbr: "<span color='#${abbr}'><b>{}</b></span>";
-  mkSpanWorkspace = abbr: "<span color='#${abbr}'><b>{icon}</b></span>";
-  server = "127.0.0.1";
-  port = 6601;
-in
 {
-  home.packages = [ pkgs.mpc ];
-  programs.waybar = {
-    enable = true;
-    systemd.enable = true;
-    settings = {
-      mainBar = {
-        layer = "top";
-        position = "top";
-        exclusive = true;
-        fixed-center = true;
-        gtk-layer-shell = true;
-        spacing = 10;
-        margin-top = 0;
-        margin-bottom = 0;
-        margin-left = 0;
-        margin-right = 0;
-        modules-left = [
-          "idle_inhibitor"
-          "niri/workspaces"
-          "niri/window"
-        ];
-        modules-right = [
-          "tray"
-          "mpd"
-          "wireplumber"
-          "niri/language"
-          "battery"
-          "clock"
-        ];
 
-        "niri/workspaces" = {
-          format = mkSpanWorkspace "${config.lib.stylix.colors.base07}";
-          on-click = "activate";
-          all-outputs = true;
-          format-icons = {
-            default = "";
-            active = "";
-          };
-        };
+  home-manager.users.${username} =
+    {
+      lib,
+      pkgs,
+      inputs,
+      ...
+    }:
+    let
+      mkSpan = abbr: "<span color='#${abbr}'><b>{}</b></span>";
+      mkSpanWorkspace = abbr: "<span color='#${abbr}'><b>{icon}</b></span>";
+      server = "127.0.0.1";
+      port = 6601;
+    in
+    {
+      home.packages = [ pkgs.mpc ];
+      programs.waybar = {
+        enable = true;
+        systemd.enable = true;
+        settings = {
+          mainBar = {
+            layer = "top";
+            position = "top";
+            exclusive = true;
+            fixed-center = true;
+            gtk-layer-shell = true;
+            spacing = 10;
+            margin-top = 0;
+            margin-bottom = 0;
+            margin-left = 0;
+            margin-right = 0;
 
-        idle_inhibitor = {
-          "format" = "{icon}";
-          format-icons = {
-            activated = "";
-            deactivated = "";
-          };
-        };
-
-        "niri/window" = {
-          separate-outputs = true;
-          icon = true;
-          icon-size = 20;
-        };
-
-        clock = {
-          calendar = {
-            format = {
-              days = mkSpan "${config.lib.stylix.colors.base0A}";
-              months = mkSpan "${config.lib.stylix.colors.base0D}";
-              today = mkSpan "${config.lib.stylix.colors.base0D}";
-              weekdays = mkSpan "${config.lib.stylix.colors.base05}";
+            modules-left = [
+              "ext/workspaces"
+              "dwl/window"
+            ];
+            "ext/workspaces" = {
+              format = "{icon}";
+              ignore-hidden = true;
+              on-click = "activate";
+              on-click-right = "deactivate";
+              sort-by-id = true;
             };
-            mode = "month";
-            on-scroll = 1;
-          };
-          format = "{:%H:%M}";
-          tooltip-format = "{calendar}";
-        };
+            "dwl/window" = {
+              format = "[{layout}] {title}";
+            };
 
-        tray = {
-          spacing = 10;
-        };
+            # modules-left = [
+            #   "idle_inhibitor"
+            #   "niri/workspaces"
+            #   "niri/window"
+            # ];
+            # modules-right = [
+            #   "tray"
+            #   "mpd"
+            #   "wireplumber"
+            #   "niri/language"
+            #   "battery"
+            #   "clock"
+            # ];
 
-        mpd = {
-          inherit server port;
-          format = "{stateIcon} {randomIcon}{repeatIcon}{singleIcon}{artist} - {title} ({elapsedTime:%H:%M}/{totalTime:%H:%M})";
-          on-click = "${lib.getExe pkgs.mpc} --host ${server} -p ${toString port} toggle";
-          unknown-tag = "";
-          random-icons = {
-            on = " ";
-          };
-          repeat-icons = {
-            on = " ";
-          };
-          single-icons = {
-            on = "1 ";
-          };
-          state-icons = {
-            paused = "";
-            playing = "";
-          };
-        };
+            # "niri/workspaces" = {
+            #   format = mkSpanWorkspace "${config.lib.stylix.colors.base07}";
+            #   on-click = "activate";
+            #   all-outputs = true;
+            #   format-icons = {
+            #     default = "";
+            #     active = "";
+            #   };
+            # };
 
-        "niri/language" = {
-          format = "{shortDescription} ";
-          on-click = "niri msg action switch-layout next";
-        };
+            # idle_inhibitor = {
+            #   "format" = "{icon}";
+            #   format-icons = {
+            #     activated = "";
+            #     deactivated = "";
+            #   };
+            # };
 
-        wireplumber = {
-          format = "{volume}% {icon}";
-          format-muted = "";
-          on-click = "${lib.getExe pkgs.pwvucontrol}";
-          format-icons = [
-            ""
-            ""
-            ""
-          ];
-        };
+            # "niri/window" = {
+            #   separate-outputs = true;
+            #   icon = true;
+            #   icon-size = 20;
+            # };
 
-        battery = {
-          states = {
-            warning = 30;
-            critical = 15;
+            # clock = {
+            #   calendar = {
+            #     format = {
+            #       days = mkSpan "${config.lib.stylix.colors.base0A}";
+            #       months = mkSpan "${config.lib.stylix.colors.base0D}";
+            #       today = mkSpan "${config.lib.stylix.colors.base0D}";
+            #       weekdays = mkSpan "${config.lib.stylix.colors.base05}";
+            #     };
+            #     mode = "month";
+            #     on-scroll = 1;
+            #   };
+            #   format = "{:%H:%M}";
+            #   tooltip-format = "{calendar}";
+            # };
+
+            # tray = {
+            #   spacing = 10;
+            # };
+
+            # mpd = {
+            #   inherit server port;
+            #   format = "{stateIcon} {randomIcon}{repeatIcon}{singleIcon}{artist} - {title} ({elapsedTime:%H:%M}/{totalTime:%H:%M})";
+            #   on-click = "${lib.getExe pkgs.mpc} --host ${server} -p ${toString port} toggle";
+            #   unknown-tag = "";
+            #   random-icons = {
+            #     on = " ";
+            #   };
+            #   repeat-icons = {
+            #     on = " ";
+            #   };
+            #   single-icons = {
+            #     on = "1 ";
+            #   };
+            #   state-icons = {
+            #     paused = "";
+            #     playing = "";
+            #   };
+            # };
+
+            # "niri/language" = {
+            #   format = "{shortDescription} ";
+            #   on-click = "niri msg action switch-layout next";
+            # };
+
+            # wireplumber = {
+            #   format = "{volume}% {icon}";
+            #   format-muted = "";
+            #   on-click = "${lib.getExe pkgs.pwvucontrol}";
+            #   format-icons = [
+            #     ""
+            #     ""
+            #     ""
+            #   ];
+            # };
+
+            # battery = {
+            #   states = {
+            #     warning = 30;
+            #     critical = 15;
+            #   };
+            #   format = "{icon}";
+            #   format-alt = "{capacity}% {icon}";
+            #   format-icons = [
+            #     ""
+            #     ""
+            #     ""
+            #     ""
+            #     ""
+            #   ];
+            #   max-length = 25;
+            # };
           };
-          format = "{icon}";
-          format-alt = "{capacity}% {icon}";
-          format-icons = [
-            ""
-            ""
-            ""
-            ""
-            ""
-          ];
-          max-length = 25;
         };
+        # style = ''
+        #   #waybar {
+        #       font-weight: bold;
+        #   }
+        #   #idle_inhibitor {
+        #     padding: 0 10px;
+        #   }
+        #   .modules-right {
+        #     padding: 0 10px;
+        #   }
+        # '';
       };
     };
-    style = ''
-      #waybar {
-          font-weight: bold;
-      }
-      #idle_inhibitor {
-        padding: 0 10px;
-      }
-      .modules-right {
-        padding: 0 10px;
-      }
-    '';
-  };
 }
