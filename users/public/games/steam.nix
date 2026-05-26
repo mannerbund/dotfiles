bb70f9107e37f7f51dec7a45c0208f7215295405
mannerbund <apostalimus@gmail.com>
2026-05-26 21:32:55 +0300

    steam: config for steam added

---
+++ users/public/games/steam.nix
@@ -0,0 +1,14 @@
+{ username, ... }:
+{
+  programs.steam.enable = true;
+
+  home-manager.users.${username} = {
+    home.persistence."/persist" = {
+      directories = [
+        ".steam"
+        ".local/share/Steam"
+        ".local/share/Paradox Interactive"
+      ];
+    };
+  };
+}
