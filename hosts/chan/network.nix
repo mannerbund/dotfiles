619f3e4ec705ea6cf8857e8f26a2fce58bfc423b
mannerbund <apostalimus@gmail.com>
2026-05-26 21:59:17 +0300

    syntax fix

--- hosts/chan/network.nix
+++ hosts/chan/network.nix
@@ -35,7 +35,7 @@
             iifname { "enp0s31f6", "wlan0" } ip daddr 224.0.0.0/4 udp dport 53317 accept
             #iifname { "enp0s31f6", "wlan0" } tcp dport 1200 accept comment "Python TCP HTTP server"
             #iifname { "enp0s31f6", "wlan0" } udp dport 1200 accept comment "Python UDP"
-            tcp dport 58530 iffname "tailscale0" accept comment "accept Tailscale SSH"
+            tcp dport 58530 iifname "tailscale0" accept comment "accept Tailscale SSH"
             tcp dport 22000 accept comment "Syncthing"
             udp dport { 22000, 21027 } accept comment "Syncthing"
         	counter comment "count dropped packets"
@@ -57,22 +57,27 @@
     };
   };
 
-  systemd.network = {
-    enable = true;
-    wait-online.enable = false;
-    networks = {
-      "10-wireless" = {
-        name = "wlan0";
-        DHCP = "yes";
-        dhcpConfig.UseDNS = false;
-      };
-      "20-ethernet" = {
-        name = "enp0s31f6";
-        DHCP = "yes";
-        dhcpConfig.UseDNS = false;
-        linkConfig.RequiredForOnline = "no";
+  systemd = {
+    network = {
+      enable = true;
+      wait-online.enable = false;
+      networks = {
+        "10-wireless" = {
+          name = "wlan0";
+          DHCP = "yes";
+          dhcpConfig.UseDNS = false;
+        };
+        "20-ethernet" = {
+          name = "enp0s31f6";
+          DHCP = "yes";
+          dhcpConfig.UseDNS = false;
+          linkConfig.RequiredForOnline = "no";
+        };
       };
     };
+    services.tailscaled.serviceConfig.Environment = [
+      "TS_DEBUG_FIREWALL_MODE=nftables"
+    ];
   };
 
   boot.initrd.systemd.network.wait-online.enable = false;
@@ -87,16 +92,11 @@
         PermitRootLogin = "no";
         UseDns = true;
       };
-      tailscale = {
-        enable = true;
-        port = 41641;
-      };
-
     };
-
-    systemd.services.tailscaled.serviceConfig.Environment = [
-      "TS_DEBUG_FIREWALL_MODE=nftables"
-    ];
+    tailscale = {
+      enable = true;
+      port = 41641;
+    };
 
     dnscrypt-proxy = {
       enable = true;
@@ -126,7 +126,75 @@
           };
         };
         cloaking_rules = pkgs.writeText "cloaking-rules.txt" ''
+          ab.chatgpt.com 45.155.204.190
+          ab.chatgpt.com 37.230.192.51
+          chatgpt.com 45.155.204.190
+          chatgpt.com 37.230.192.51
+          operator.chatgpt.com 45.155.204.190
+          operator.chatgpt.com 37.230.192.51
+          sora.chatgpt.com 45.155.204.190
+          sora.chatgpt.com 37.230.192.51
+          webrtc.chatgpt.com 45.155.204.190
+          webrtc.chatgpt.com 37.230.192.51
+          www.chatgpt.com 45.155.204.190
+          www.chatgpt.com 37.230.192.51
 
+          android.chat.openai.com 45.155.204.190
+          android.chat.openai.com 37.230.192.51
+          api.openai.com 45.155.204.190
+          api.openai.com 37.230.192.51
+          auth.openai.com 45.155.204.190
+          auth.openai.com 37.230.192.51
+          auth0.openai.com 45.155.204.190
+          auth0.openai.com 37.230.192.51
+          blog.openai.com 45.155.204.190
+          blog.openai.com 37.230.192.51
+          cdn.openai.com 45.155.204.190
+          cdn.openai.com 37.230.192.51
+          chat.openai.com 45.155.204.190
+          chat.openai.com 37.230.192.51
+          community.openai.com 45.155.204.190
+          community.openai.com 37.230.192.51
+          help.openai.com 45.155.204.190
+          help.openai.com 37.230.192.51
+          ios.chat.openai.com 45.155.204.190
+          ios.chat.openai.com 37.230.192.51
+          openai.com 45.155.204.190
+          openai.com 37.230.192.51
+          platform.api.openai.com 45.155.204.190
+          platform.api.openai.com 37.230.192.51
+          platform.openai.com 45.155.204.190
+          platform.openai.com 37.230.192.51
+          tcr9i.chat.openai.com 45.155.204.190
+          tcr9i.chat.openai.com 37.230.192.51
+          videos.openai.com 45.155.204.190
+          videos.openai.com 37.230.192.51
+          www.openai.com 45.155.204.190
+          www.openai.com 37.230.192.51
+          arena.openai.com 45.155.204.190
+          arena.openai.com 37.230.192.51
+          beta.api.openai.com 45.155.204.190
+          beta.api.openai.com 37.230.192.51
+          beta.openai.com 45.155.204.190
+          beta.openai.com 37.230.192.51
+          contest.openai.com 45.155.204.190
+          contest.openai.com 37.230.192.51
+          debate-game.openai.com 45.155.204.190
+          debate-game.openai.com 37.230.192.51
+          discuss.openai.com 45.155.204.190
+          discuss.openai.com 37.230.192.51
+          gym.openai.com 45.155.204.190
+          gym.openai.com 37.230.192.51
+          jukebox.openai.com 45.155.204.190
+          jukebox.openai.com 37.230.192.51
+          labs.openai.com 45.155.204.190
+          labs.openai.com 37.230.192.51
+          microscope.openai.com 45.155.204.190
+          microscope.openai.com 37.230.192.51
+          spinningup.openai.com 45.155.204.190
+          spinningup.openai.com 37.230.192.51
+          universe.openai.com 45.155.204.190
+          universe.openai.com 37.230.192.51
         '';
       };
     };
